7937fe412345424a9352dad8c97ed1cc5c0e605c
farajzada <mfarajzadaa@gmail.com>
2024-01-17 01:45:10 +0400

    eolymp task attached

---
+++ src/main/java/conditionsLoopsHomeWork/SimpleTask.java
@@ -0,0 +1,12 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class SimpleTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a / 10);
+        System.out.println(a % 10);
+    }
+}
