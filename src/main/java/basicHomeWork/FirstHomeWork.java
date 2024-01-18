6afb27df037733983e9f19d0997487fd003a6fdc
farajzada <mfarajzadaa@gmail.com>
2024-01-18 11:58:48 +0400

    eolymp task attached

--- src/main/java/basicHomeWork/FirstHomeWork.java
+++ src/main/java/basicHomeWork/FirstHomeWork.java
@@ -1,7 +1,7 @@
 package basicHomeWork;
 
-public class Main {
+public class FirstHomeWork {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1\n2\n3\n4\n5");
     }
 }
