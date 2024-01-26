34bc65be30fa727aa16b82c9f3b66a56367e01ee
farajzada <mfarajzadaa@gmail.com>
2024-01-26 15:31:31 +0400

    MinMax.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/MinMax.java
@@ -0,0 +1,13 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(Math.min(a, b) + " " + Math.max(a, b));
+
+    }
+}
