de8332eea712f999dd8e5232bc46531821a475f6
farajzada <mfarajzadaa@gmail.com>
2024-01-30 17:42:29 +0400

    DivisibilityByNumber.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/DivisibilityByNumber.java
@@ -0,0 +1,17 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class DivisibilityByNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if (n % a == 0 && n % b == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
