6c1dc6bbcbe007b41b9ad7f9e7fb3558c489b80e
farajzada <mfarajzadaa@gmail.com>
2024-01-30 16:26:44 +0400

    UseTheFunction.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/UseTheFunction.java
@@ -0,0 +1,17 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class UseTheFunction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double z = sc.nextDouble();
+        double maxXY = Math.max(x, y);
+        double maxYZ = Math.max(y, z);
+        double sum = x + y + z;
+        double min = Math.min(maxXY, maxYZ);
+        System.out.printf("%.2f", Math.min(sum, min));
+    }
+}
