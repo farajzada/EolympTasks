8feff8bd2510de97275e6123f28b7b62c6060563
farajzada <mfarajzadaa@gmail.com>
2024-01-30 16:07:47 +0400

    PositiveNegativeZero.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n > 0) {
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
