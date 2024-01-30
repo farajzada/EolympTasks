a1226e0c395e347903a230bc1d7beecf70d9c4bf
farajzada <mfarajzadaa@gmail.com>
2024-01-30 17:32:47 +0400

    ConditionalStatement1.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/ConditionalStatement1.java
@@ -0,0 +1,20 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class ConditionalStatement1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+
+        int firstCondition = (x * x) - (3 * x) + 4;
+        int secondCondition = x + 7;
+
+        if (x < 5) {
+            System.out.println(firstCondition);
+        } else {
+            System.out.println(secondCondition);
+        }
+    }
+}
