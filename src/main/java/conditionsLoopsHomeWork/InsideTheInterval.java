4486da7655ad1214edceddbc1d8e9c67619378ab
farajzada <mfarajzadaa@gmail.com>
2024-02-01 12:25:20 +0400

    InsideTheInterval.java attached

---
+++ src/main/java/conditionsLoopsHomeWork/InsideTheInterval.java
@@ -0,0 +1,19 @@
+package conditionsLoopsHomeWork;
+
+import java.util.Scanner;
+
+public class InsideTheInterval {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (x >= a && x <= b) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
